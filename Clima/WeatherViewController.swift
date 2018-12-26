b16558d742e2d161f1263a36338cb27e993a489e
m0nq <forgetmenaught@gmail.com>
2018-12-26 14:54:32 -0800

    Update UI icon image with cooresponding weather icon

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -59,6 +59,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             weatherDataModel.city = json["name"].stringValue
             weatherDataModel.condition = json["weather"][0]["id"].intValue
             weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            updateUIWithWeatherData()
         } else {
             cityLabel.text = "Weather Unavailable"
         }
@@ -68,6 +69,11 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 /***************************************************************/
 
 //Write the updateUIWithWeatherData method here:
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = String(weatherDataModel.temperature)
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+    }
 
 //MARK: - Location Manager Delegate Methods
 /***************************************************************/
