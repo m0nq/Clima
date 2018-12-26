fd691df80d5abeb0976c0b41148496e1caa0777a
m0nq <forgetmenaught@gmail.com>
2018-12-26 12:39:50 -0800

    Add location manager callbacks for success or error

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
-    
+
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "22e1b8e0ea28fb5a49762870dd1c9483"
 
@@ -27,11 +27,12 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     //MARK: - Networking
     /***************************************************************/
-    
+
     //Write the getWeatherData method here:
 
     //MARK: - JSON Parsing
@@ -48,8 +49,22 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     /***************************************************************/
 
     //Write the didUpdateLocations method here:
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            let params: [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+        }
+    }
 
     //Write the didFailWithError method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
 
     //MARK: - Change City Delegate methods
     /***************************************************************/
