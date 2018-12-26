d4598437b475586335336b9e0b76cc4b4673f7f8
m0nq <forgetmenaught@gmail.com>
2018-12-26 12:06:29 -0800

    Update the plist file to accept location usage privileges

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "22e1b8e0ea28fb5a49762870dd1c9483"
 
     //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
 
     //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
@@ -23,7 +24,9 @@ class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
     }
 
     //MARK: - Networking
