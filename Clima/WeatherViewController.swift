5d098755f564a6e19986207c01e20c3cc84a9072
m0nq <forgetmenaught@gmail.com>
2018-12-26 23:57:59 -0800

    Weather view controller label updates to home when tapped

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -31,6 +31,12 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
         locationManager.startUpdatingLocation()
     }
 
+    @IBAction func homeWeatherButton(_ sender: UIButton) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.startUpdatingLocation()
+    }
+
     //MARK: - Location Manager Delegate Methods
     /***************************************************************/
 
@@ -92,7 +98,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     //Write the updateUIWithWeatherData method here:
     func updateUIWithWeatherData() {
         cityLabel.text = weatherDataModel.city
-        temperatureLabel.text = String(weatherDataModel.temperature)
+        temperatureLabel.text = "\(weatherDataModel.temperature)°"
         weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
     }
 
@@ -102,7 +108,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
 
     //Write the userEnteredANewCityName Delegate method here:
     func userEnteredANewCityName(city: String) {
-        print("city -> \(city)")
+        let params: [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
     }
 
     //Write the PrepareForSegue Method here
